7b1d38b3b6497bef05f5bbaa0f81700253643422
Kisha Prudente <kisha.prudente@gmail.com>
2020-06-09 13:46:38 -0400

    installed jest and babel-jest

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
-const path = require("path");
-const router = require("express").Router();
+const path = require('path');
+const router = require('express').Router();
 // const apiRoutes = require("./api");
 
 // API Routes
@@ -7,7 +7,7 @@ const router = require("express").Router();
 
 // If no API routes are hit, send the React app
 router.use(function (req, res) {
-	res.sendFile(path.join(__dirname, "../client/build/index.html"));
+	res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 module.exports = router;
