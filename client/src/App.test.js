22d05a7ed43686ba881f14058aedbaa1aa3564e0
Kisha Prudente <kisha.prudente@gmail.com>
2020-06-09 15:11:36 -0400

    fix app test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-	const { getByText } = render(<App />);
-	const textElement = getByText(/Hello World/);
-	expect(textElement).toBeInTheDocument();
+  const { getByText } = render(<App />);
+  const textElement = getByText('Hello World');
+  expect(textElement).toBeInTheDocument();
 });
