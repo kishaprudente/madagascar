9d0f2137bdbe2aef5fe43c3fb0902d06168ed8cb
Jiyoon Kim <jiyoon9886@gmail.com>
2020-06-09 17:46:14 -0400

    created landing page and added fonts to index.css

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React from 'react';
+import Landing from './pages/Landing';
 
 function App() {
-	return <div className="App">HELLO WORLD</div>;
+  return <Landing />;
 }
 
 export default App;
