f96966a65c10e7d739756e5e27627d29b6bf1ac8
Kisha Prudente <kisha.prudente@gmail.com>
2020-06-09 21:12:11 -0400

    fixed and moved app test

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React from 'react';
 
 function App() {
-	return <div className="App">HELLO WORLD</div>;
+  return (
+    <div className="App" data-testid="app">
+      HELLO WORLD
+    </div>
+  );
 }
 
 export default App;
