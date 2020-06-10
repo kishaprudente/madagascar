f61c20922cb8b1b7dfaaafb3db6012abfe5ca48d
Jiyoon Kim <jiyoon9886@gmail.com>
2020-06-10 12:27:58 -0400

    fixed import route for Chirpy

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Grid, Button, TextField } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import chirpy from './chirpy.svg';
+import chirpy from '../assets/chirpy.svg';
 
 export default function Signup() {
   return (
