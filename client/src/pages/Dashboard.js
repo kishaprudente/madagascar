f51b25c5e0a6d3e0e6c46d568a20584375bc1903
Jiyoon Kim <jiyoon9886@gmail.com>
2020-06-10 12:29:09 -0400

    created Dashboard file with route for Chirpy

---
+++ client/src/pages/Dashboard.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import { Grid, Button, TextField } from '@material-ui/core';
+//import { makeStyles } from '@material-ui/core/styles';
+import chirpy from '../assets/chirpy.svg';
+
+export default function Dashboard() {
+  return (
+    <Grid
+      container
+      style={container}
+      justify='space-evenly'
+      alignItems='center'
+      direction='column'
+    >
+      <Grid item />
+      <Grid item>
+        <img src={chirpy} alt='chirpy the bird' />
+      </Grid>
+
+      <Grid item style={{ width: '180px' }}>
+        <TextField
+          style={{ marginBottom: '10px' }}
+          label='Username'
+          id='outlined-size-normal'
+          defaultValue='Username'
+          variant='outlined'
+        ></TextField>
+        <TextField
+          style={{ marginBottom: '10px' }}
+          label='Password'
+          id='outlined-size-normal'
+          defaultValue='Password'
+          variant='outlined'
+        ></TextField>
+      </Grid>
+
+      <Grid item>
+        <Button style={buttonStyle} variant='contained' color='primary'>
+          Login
+        </Button>
+      </Grid>
+
+      <Grid item>
+        <a href='#'>No account? Sign up!</a>
+      </Grid>
+      <Grid item></Grid>
+    </Grid>
+  );
+}
+
+const container = {
+  backgroundColor: '#A1D1B6',
+  width: '100vw',
+  height: '100vh',
+  flexGrow: '1',
+};
+
+const chirpyStyle = {
+  top: '185px',
+  textAlign: 'center',
+  left: '40%',
+  right: '40%',
+  width: '4em',
+  height: '4em',
+};
+
+const buttonStyle = {
+  color: 'black',
+  fontSize: 16,
+  border: '1px solid',
+  lineHeight: 1.5,
+  backgroundColor: 'rgba(255, 216, 99, 0.87)',
+  fontFamily: 'Reenie Beanie',
+};
