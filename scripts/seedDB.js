f0bdddc886bc25767a10b2b31a30769a51f8e0a2
Kisha Prudente <kisha.prudente@gmail.com>
2020-06-11 16:05:21 -0400

    fix conflict

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const ObjectID = require('mongodb').ObjectID;
 const db = require('../models');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/madagascardb');
@@ -33,32 +32,20 @@ const postSeed = [
     mood: 'Happy',
     post: 'I feel energetic.',
     date: new Date(Date.now()),
-<<<<<<< HEAD
-    sent: false,
-=======
     sent: true
->>>>>>> master
   },
   {
     mood: 'Strong',
     post: 'I feel confident.',
     date: new Date(Date.now()),
-<<<<<<< HEAD
-    sent: false,
-=======
     sent: true
->>>>>>> master
   },
   {
     mood: 'Lonely',
     post: 'I feel alone.',
     date: new Date(Date.now()),
     sent: true,
-<<<<<<< HEAD
     reply: '5ee19956e8f55c23a6f7eb2c'
-=======
-    reply: mongoose.Types.ObjectId('5ee27130a90b1edeed22fbd9')
->>>>>>> master
   }
 ];
 
