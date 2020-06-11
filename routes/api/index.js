bcec5f771f8e82a3aaebc6206d039c7a78c24cb3
Kisha Prudente <kisha.prudente@gmail.com>
2020-06-11 18:02:46 -0400

    moved user api route to its own folder

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,17 +1,9 @@
 const router = require('express').Router();
 const postRoutes = require('./posts');
-const userRoutes = require('./user');
 const replyRoutes = require('./replies');
 
 //Routes
 router.use('/posts', postRoutes);
 router.use('/replies', replyRoutes);
-router.use('/', userRoutes);
-
-// Route for logging user out
-router.get('/logout', (req, res) => {
-  req.logout();
-  res.redirect('/');
-});
 
 module.exports = router;
