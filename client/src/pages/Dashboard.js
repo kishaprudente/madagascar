e42b7a6606e2a4baf869f394e16818a640da2761
Jiyoon Kim <jiyoon9886@gmail.com>
2020-06-10 22:21:52 -0400

    hooked up getPost to expansion panels

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   Grid,
   Box,
@@ -10,15 +10,30 @@ import {
 } from '@material-ui/core';
 //import { makeStyles } from '@material-ui/core/styles';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import API from '../utils/API.js';
 import chirpy from '../assets/chirpy.svg';
 
 const Dashboard = () => {
-  const [feelsToSend, setFeelsToSend] = useState('');
+  const [post, setPost] = useState('');
+  const [posts, setPosts] = useState([]);
 
   const handleInputChange = (event) => {
     const { value } = event.target;
-    setFeelsToSend(value);
+    setPost(value);
   };
+
+  const loadPosts = async () => {
+    try {
+      const allPosts = await API.getPost();
+      setPosts(allPosts.data);
+    } catch (err) {
+      throw err;
+    }
+  };
+
+  useEffect(() => {
+    loadPosts();
+  }, [posts]);
 
   return (
     <Grid
@@ -60,65 +75,30 @@ const Dashboard = () => {
           Send
         </Button>
       </Grid>
+
       <Grid item style={{ marginLeft: '20px', marginRight: '20px' }}>
-        <Box component='div' style={{ height: '380px' }} overflow='auto'>
-          <ExpansionPanel>
-            <ExpansionPanelSummary
-              expandIcon={<ExpandMoreIcon />}
-              aria-controls='panel1a-content'
-              id='panel1a-header'
-            >
-              Date Time Hello
-            </ExpansionPanelSummary>
-            <ExpansionPanelDetails>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-              eget.
-            </ExpansionPanelDetails>
-          </ExpansionPanel>
-          <ExpansionPanel>
-            <ExpansionPanelSummary
-              expandIcon={<ExpandMoreIcon />}
-              aria-controls='panel1a-content'
-              id='panel1a-header'
-            >
-              Date Time Hello
-            </ExpansionPanelSummary>
-            <ExpansionPanelDetails>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-              eget.
-            </ExpansionPanelDetails>
-          </ExpansionPanel>
-          <ExpansionPanel>
-            <ExpansionPanelSummary
-              expandIcon={<ExpandMoreIcon />}
-              aria-controls='panel1a-content'
-              id='panel1a-header'
-            >
-              Date Time Hello
-            </ExpansionPanelSummary>
-            <ExpansionPanelDetails>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-              eget.
-            </ExpansionPanelDetails>
-          </ExpansionPanel>
-          <ExpansionPanel>
-            <ExpansionPanelSummary
-              expandIcon={<ExpandMoreIcon />}
-              aria-controls='panel1a-content'
-              id='panel1a-header'
-            >
-              Date Time Hello
-            </ExpansionPanelSummary>
-            <ExpansionPanelDetails>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-              eget.
-            </ExpansionPanelDetails>
-          </ExpansionPanel>
-        </Box>
+        {posts.length ? (
+          <Box component='div' style={{ height: '380px' }} overflow='auto'>
+            {posts.map((post) => {
+              return (
+                <ExpansionPanel>
+                  <ExpansionPanelSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls='panel1a-content'
+                    id='panel1a-header'
+                    key={post.id}
+                  >
+                    {post.date}
+                    {post.mood}
+                  </ExpansionPanelSummary>
+                  <ExpansionPanelDetails>{post.post}</ExpansionPanelDetails>
+                </ExpansionPanel>
+              );
+            })}
+          </Box>
+        ) : (
+          <h3>You don't have any posts yet!</h3>
+        )}
       </Grid>
     </Grid>
   );
@@ -136,10 +116,6 @@ const container = {
 };
 
 const chirpyStyle = {
-  //top: '185px',
-  //textAlign: 'center',
-  //left: '40%',
-  //right: '40%',
   width: '1em',
   height: '1em',
 };
