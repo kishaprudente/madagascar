4cf47b8b6a201ce5299179515300b1495d3067f1
kwu3 <kwu3@villanova.edu>
2020-06-10 23:05:34 -0400

    Refactored to single quotes

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,19 +1,19 @@
-import axios from "axios";
+import axios from 'axios';
 
 export default {
   // Get all post 
-  getPost: function () {
-    return axios.get("/api/posts");
+  getPost: function() {
+    return axios.get('/api/posts');
   },
-  createPost: function (postData) {
-    return axios.post("/api/posts", postData);
+  createPost: function(postData) {
+    return axios.post('/api/posts', postData);
   },
   // Deletes the post with the given id
-  deletePost: function (id) {
-    return axios.delete("/api/posts/" + id);
+  deletePost: function(id) {
+    return axios.delete('/api/posts/' + id);
   },
   // Saves a post to the database
-  replyPost: function (replyData) {
-    return axios.post("/api/replys", replyData);
+  replyPost: function(replyData) {
+    return axios.post('/api/replys', replyData);
   }
 };
