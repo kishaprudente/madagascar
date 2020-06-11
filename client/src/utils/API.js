70190c9c0ce2caecbf893527f1bb931119bdb1d8
kwu3 <kwu3@villanova.edu>
2020-06-10 23:02:51 -0400

    Created post route for posts

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,15 +2,18 @@ import axios from "axios";
 
 export default {
   // Get all post 
-  getPost: function() {
+  getPost: function () {
     return axios.get("/api/posts");
   },
+  createPost: function (postData) {
+    return axios.post("/api/posts", postData);
+  },
   // Deletes the post with the given id
-  deletePost: function(id) {
+  deletePost: function (id) {
     return axios.delete("/api/posts/" + id);
   },
   // Saves a post to the database
-  replyPost: function(replyData) {
-    return axios.post("/api/posts", replyData);
+  replyPost: function (replyData) {
+    return axios.post("/api/replys", replyData);
   }
 };
