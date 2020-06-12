9242acda6e022c43f181abcd7b82ee6a138f4a66
Kaitlyn Rodriguez <kaitlyn.l.rodriguez@gmail.com>
2020-06-12 17:23:06 -0400

    Pushing test

--- test/apiRoutes.test.js
+++ test/apiRoutes.test.js
@@ -1,5 +1,7 @@
 const axios = require('axios');
 
+
+//Two GET Tests for api/post and api/replies
 describe('API Routes', () => {
   describe('GET /api/posts', () => {
     // - should have a 200 status code
@@ -22,32 +24,34 @@ describe('API Routes', () => {
     });
 
   });
-});
 
-describe('API Routes', () => {
-  describe('POST Endpoints', () => {
-    it('should create a new post', async ()=> {
-      const { status } = await axios.post('/api/replies',
-        {
-          response: 'Test Jest Post',
-          post:'5ee1a85254d653c1078a5932'
-        });
-      expect(status).toBe(200);
-      console.log(status);
+  //Two POST Tests for api/post and api/replies
+  describe('API Routes', () => {
+    describe('POST Endpoints', () => {
+      it('should create a new reply', async ()=> {
+        const { status } = await axios.post('/api/replies',
+          {
+            response: 'Test Jest Post',
+            post:'5ee1a85254d653c1078a5932'
+          });
+        expect(status).toBe(200);
+        console.log(status);
+      });
+    });
+    describe('POST Endpoints', () => {
+      it('should create a new post', async ()=> {
+        const { status } = await axios.post('/api/post',
+          {
+            mood: 'Test Jest Post',
+            post:'5ee1a85254d653c1078a5932',
+            date: '2020-06-11 23:35:10.588Z',
+            sent: 'false',
+            reply: '5ee27130a90b1edeed22fbd9'
+          });
+        expect(status).toBe(200);
+        console.log(status);
+      });
     });
   });
-  describe('POST Endpoints', () => {
-    it('should create a new post', async ()=> {
-      const { status } = await axios.post('/api/post',
-        {
-          mood: 'Test Jest Post',
-          post:'5ee1a85254d653c1078a5932',
-          date: '2020-06-11 23:35:10.588Z',
-          sent: 'false',
-          reply: '5ee27130a90b1edeed22fbd9'
-        });
-      expect(status).toBe(200);
-      console.log(status);
-    });
-  });
+
 });
