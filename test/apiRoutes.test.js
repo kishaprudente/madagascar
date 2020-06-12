4f7aff135a9f389cbc53b892853de8f1808d5622
Kaitlyn Rodriguez <kaitlyn.l.rodriguez@gmail.com>
2020-06-12 16:57:17 -0400

    Adding a post test

--- test/apiRoutes.test.js
+++ test/apiRoutes.test.js
@@ -6,43 +6,37 @@ describe('API Routes', () => {
     it('should have a 200 status code', async () => {
       // make a get request & capture the response
       const { status } = await axios.get('/api/posts');
-
-      // expect the response.status to be 200
-      expect(status).toBe(200);
-    });
-
-    it('should have a 200 status code', async () => {
-      // make a get request & capture the response
-      const { status } = await axios.get('/api/posts/:id');
-
       // expect the response.status to be 200
       expect(status).toBe(200);
     });
 
   });
 
-  //API routes for replies
   describe('GET /api/replies', () => {
     // - should have a 200 status code
     it('should have a 200 status code', async () => {
-      // make a get request
-      // capture the response
-      const { status } = await axios.get('/api/replies');
-      // expect the response.status to be 200
-      expect(status).toBe(200);
-
-    });
-
-    // });
-    it('should have a 200 status code', async () => {
       // make a get request & capture the response
-      const { status } = await axios.get('/api/replies/:id');
-
+      const { status } = await axios.get('/api/replies');
       // expect the response.status to be 200
       expect(status).toBe(200);
     });
 
   });
-
 });
 
+describe('API Routes', () => {
+  describe('POST Endpoints', () => {
+    it('should create a new post', async ()=> {
+      const { status } = await axios.post('/api/replies',
+        {
+          response: 'Feel Better',
+          post:'5ee1a85254d653c1078a5932'
+        });
+      expect(status).toBe(200);
+      console.log(status);
+    });
+  });
+
+
+ 
+});
