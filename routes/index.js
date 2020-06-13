4d42cd956a841f60eb1377070597983d985f845a
Kisha Prudente <kisha.prudente@gmail.com>
2020-06-12 20:05:02 -0400

    fixed routes for auth

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const router = require('express').Router();
 const apiRoutes = require('./api');
-const authRoutes = require('./authAPI');
+const authRoutes = require('./authAPI/user');
 
 // API Routes
 router.use('/api', apiRoutes);
