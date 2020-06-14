a42c160834ab4b5d863a7b9cc0897194b9ee7327
kwu3 <kwu3@villanova.edu>
2020-06-13 21:53:32 -0400

    Put alert component in new AlertBar component

---
+++ client/src/components/AlertBar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Snackbar from '@material-ui/core/Snackbar';
+import Alert from './Alert';
+
+// types: 'error, 'warning', 'info', 'success'
+const AlertBar = ({ message, type, openState, onClose }) => {
+  return (
+    <Snackbar
+      open={openState}
+      autoHideDuration={1000}
+      onClose={() => {
+        onClose();
+      }}
+    >
+      <Alert
+        onClose={() => {
+          onClose();
+        }}
+        severity={type}
+      >
+        {message}
+      </Alert>
+    </Snackbar>
+  );
+};
+
+export default AlertBar;
