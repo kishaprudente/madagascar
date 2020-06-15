ef0d9c01d0fb1b2b7d40c270c4b256cd29bd1c14
Kisha Prudente <kisha.prudente@gmail.com>
2020-06-15 12:00:52 -0400

    changed functions to arrow functions and to populate user

--- controllers/repliesController.js
+++ controllers/repliesController.js
@@ -3,31 +3,35 @@ const db = require('../models');
 // Defining methods for the postsController
 module.exports = {
   //All post will show up and if there is a reply then it should show as well
-  findAll: function(req, res) {
+  findAll: (req, res) => {
     db.Reply
-      .find()
+      .find({})
+      .populate('post')
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
-  findById: function(req, res) {
+  findById: (req, res) => {
     db.Reply
       .findById(req.params.id)
+      .populate('post')
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
-  create: function(req, res) {
+  create: ({ body }, res) => {
     db.Reply
-      .create(req.body)
+      .create(body)
+      .then(({ _id }) => db.Post.findOneAndUpdate({}, { $set: { post: _id }}, { new: true }))
+      .then(({ _id }) => db.User.findOneAndUpdate({}, { $push: { replies: _id } }, { new: true }))
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
-  update: function(req, res) {
+  update: (req, res) => {
     db.Reply
       .findOneAndUpdate({ _id: req.params.id }, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
-  remove: function(req, res) {
+  remove: (req, res) => {
     db.Reply
       .findById({ _id: req.params.id })
       .then(dbModel => dbModel.remove())
