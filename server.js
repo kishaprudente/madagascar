1d25711f3f07a3e0ca6f4ce9610be952791195f2
kwu3 <kwu3@villanova.edu>
2020-06-14 22:46:01 -0400

    Fixed passport sessions

--- server.js
+++ server.js
@@ -1,49 +1,42 @@
 const express = require('express');
-const session = require('express-session');
-const mongoose = require('mongoose');
-const passport = require('passport');
 const bodyParser = require('body-parser');
-
+const session = require('express-session');
+// const mongoose = require('mongoose');
+const dbConnection = require('./config/db_connection');
+const MongoStore = require('connect-mongo')(session);
+const passport = require('./config/passport');
 const routes = require('./routes');
-const db = require('./config/keys').MONGO_DB;
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(
-  bodyParser.urlencoded({
-    extended: false
-  })
-);
-app.use(bodyParser.json());
+app
+  .use(bodyParser.urlencoded({ extended: false }))
+  .use(bodyParser.json())
+  // We need to use sessions to keep track of our user's login status
+  .use(
+    session({
+      secret: 'supersecretkeythatnooneknows',
+      store: new MongoStore({ mongooseConnection: dbConnection }),
+      resave: false, //required
+      saveUninitialized: false, //required
+    })
+  )
+  .use(passport.initialize())
+  .use(passport.session())
+  // Add routes, both API and view
+  .use(routes);
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
-// Add routes, both API and view
-app.use(routes);
-
 
 app.get('*', (req, res) => {
   res.status(404).send('File not found');
 });
-// We need to use sessions to keep track of our user's login status
-app.use(
-  session({ secret: 'keyboard cat', resave: true, saveUninitialized: true })
-);
-app.use(passport.initialize());
-require('./config/passport')(passport);
-
-// Connect to the Mongo DB
-mongoose
-  .connect(process.env.MONGODB_URI || db, { useNewUrlParser: true })
-  .then(() => console.log('MongoDB successfully connected'))
-  .catch((err) => console.log(err));
 
 // Start the API server
-app.listen(PORT, function () {
+app.listen(PORT, () => {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
