87785941378584bf51514351861774f3136e1f4f
Kisha Prudente <kisha.prudente@gmail.com>
2020-06-15 13:17:50 -0400

    fix api route tests

--- controllers/repliesController.js
+++ controllers/repliesController.js
@@ -4,38 +4,37 @@ const db = require('../models');
 module.exports = {
   //All post will show up and if there is a reply then it should show as well
   findAll: (req, res) => {
-    db.Reply
-      .find({})
+    db.Reply.find({})
       .populate('post')
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
   },
   findById: (req, res) => {
-    db.Reply
-      .findById(req.params.id)
+    db.Reply.findById(req.params.id)
       .populate('post')
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
   },
   create: ({ body }, res) => {
-    db.Reply
-      .create(body)
-      .then(({ _id }) => db.Post.findOneAndUpdate({}, { $set: { reply: _id }}, { new: true }))
-      .then(({ _id }) => db.User.findOneAndUpdate({}, { $push: { replies: _id } }, { new: true }))
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
+    db.Reply.create(body)
+      .then(({ _id }) =>
+        db.Post.findOneAndUpdate({}, { $set: { reply: _id } }, { new: true })
+      )
+      .then(({ _id }) =>
+        db.User.findOneAndUpdate({}, { $push: { replies: _id } }, { new: true })
+      )
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
   },
   update: (req, res) => {
-    db.Reply
-      .findOneAndUpdate({ _id: req.params.id }, req.body)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
+    db.Reply.findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
   },
   remove: (req, res) => {
-    db.Reply
-      .findById({ _id: req.params.id })
-      .then(dbModel => dbModel.remove())
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
-  }
+    db.Reply.findById({ _id: req.params.id })
+      .then((dbModel) => dbModel.remove())
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
 };
