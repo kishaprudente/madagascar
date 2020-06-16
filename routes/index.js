7b9cadb0ab25cc6b019b13f5da4709dad9df717c
Kisha Prudente <kisha.prudente@gmail.com>
2020-06-16 11:09:53 -0400

    fix paths

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ router.use('/api', apiRoutes);
 router.use('/api/auth', authRoutes);
 
 // If no API routes are hit, send the React app
-router.use(function (req, res) {
+router.use('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
