18c2ed7d57d2b14cfa7bd1cad8b36322f53acdd5
Kisha Prudente <kisha.prudente@gmail.com>
2020-07-27 14:12:02 -0400

    refactored app to use authContext

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { AuthContext } from './utils/authContext';
 
 import Reply from './pages/Reply';
 import Moodboard from './pages/Moodboard';
 import Inbox from './pages/Inbox';
-
 import Landing from './pages/Landing';
-import Login from './pages/Login';
+import Signin from './pages/Signin';
 import Signup from './pages/Signup';
-
 import BottomNav from './components/BottomNav';
-import PrivateRoute from './utils/PrivateRoute';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
-  const user = localStorage.getItem('user');
+  const existingTokens = JSON.parse(localStorage.getItem('tokens'));
+  const [authTokens, setAuthTokens] = useState(existingTokens);
+
+  const setTokens = (data) => {
+    localStorage.setItem('tokens', JSON.stringify(data));
+    setAuthTokens(data);
+  };
 
   return (
     <div data-testid='app'>
-      <Router>
-        <Switch>
-          <Route exact path='/' component={Landing} />
-          <Route exact path='/login' component={Login} />
-          <Route exact path='/signup' component={Signup} />
-          <PrivateRoute exact path='/moodboard' component={Moodboard} />
-          <PrivateRoute exact path='/reply' component={Reply} />
-          <PrivateRoute exact path='/inbox' component={Inbox} />
-        </Switch>
-        {user ? <BottomNav /> : null}
-      </Router>
+      <AuthContext.Provider value={{ authTokens, setAuthTokens: setTokens }}>
+        <Router>
+          <Switch>
+            <Route exact path='/' component={Landing} />
+            <Route exact path='/signin' component={Signin} />
+            <Route exact path='/signup' component={Signup} />
+            <PrivateRoute path='/moodboard' component={Moodboard} />
+            <PrivateRoute path='/reply' component={Reply} />
+            <PrivateRoute path='/inbox' component={Inbox} />
+          </Switch>
+          {existingTokens ? <BottomNav /> : null}
+        </Router>
+      </AuthContext.Provider>
     </div>
   );
 }
