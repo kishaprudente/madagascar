9e5915cc153466e6edae913ee105baffa38fd22e
Kaitlyn Rodriguez <kaitlyn.l.rodriguez@gmail.com>
2020-07-28 15:59:20 -0400

    SeedDB and made margin changes to About

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -11,6 +11,7 @@ import CardContent from '@material-ui/core/CardContent';
 const useStyles = makeStyles({
   root: {
     minWidth: 275,
+    margin: 5,
     border: '1px solid #000000',
     boxSizing: 'border-box',
     boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)'
