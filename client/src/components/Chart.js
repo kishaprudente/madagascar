5221d5a328b4ef1be28658d352a9789546a86699
kwu3 <kwu3@villanova.edu>
2020-07-29 20:33:37 -0400

    Used user data from db to populate graph

--- client/src/components/Chart.js
+++ client/src/components/Chart.js
@@ -1,14 +1,21 @@
 import React, { useRef, useEffect, useState } from 'react';
-import { select, axisBottom, axisLeft, scaleLinear, scaleBand } from 'd3';
+import {
+  select,
+  axisBottom,
+  axisLeft,
+  scaleLinear,
+  scaleBand,
+  format,
+} from 'd3';
 
-const Chart = () => {
-  const [data, setData] = useState([
-    { mood: 'Happy', value: 5 },
-    { mood: 'Angry', value: 7 },
-    { mood: 'Anxious', value: 5 },
-    { mood: 'Loved', value: 10 },
-    { mood: 'Sad', value: 1 },
-  ]);
+const Chart = ({ data }) => {
+  // const [data, setData] = useState([
+  //   { mood: 'Happy', value: 0 },
+  //   { mood: 'Angry', value: 0 },
+  //   { mood: 'Anxious', value: 0 },
+  //   { mood: 'Loved', value: 0 },
+  //   { mood: 'Sad', value: 0 },
+  // ]);
   const svgRef = useRef();
 
   const maxValue = Math.max(...data.map((d) => d.value));
@@ -26,7 +33,7 @@ const Chart = () => {
       .range([150, 0]);
 
     const colorScale = scaleLinear()
-      .domain([1, 10])
+      .domain([1, maxValue])
       .range(['#F3D068', 'orange'])
       .clamp(true);
 
@@ -34,10 +41,17 @@ const Chart = () => {
     svg
       .select('.x-axis')
       .style('transform', 'translateY(150px)')
+      .style('font-family', 'ruluko')
       .call(xAxis);
 
-    const yAxis = axisLeft(yScale);
-    svg.select('.y-axis').call(yAxis);
+    const yAxisTicks = yScale.ticks().filter((tick) => Number.isInteger(tick));
+    const yAxis = axisLeft(yScale)
+      .tickValues(yAxisTicks)
+      .tickFormat(format('d'));
+    svg
+      .select('.y-axis')
+      .style('font-family', 'ruluko')
+      .call(yAxis);
 
     svg
       .selectAll('.bar')
@@ -46,12 +60,16 @@ const Chart = () => {
       .attr('class', 'bar')
       .style('transform', 'scale(1, -1)')
       .attr('x', (d) => xScale(d.mood))
-      .attr('y', -150)
       .attr('width', xScale.bandwidth())
-      .attr('height', (d) => 150 - yScale(d.value))
-      .transition()
+      .attr('y', -150)
       .attr('fill', (d) => colorScale(d.value))
-      .attr('height', (d) => 150 - yScale(d.value));
+      .transition()
+      .duration(500)
+      .attr('height', (d) => 150 - yScale(d.value))
+      .delay((d, i) => {
+        console.log(i);
+        return i * 100;
+      });
   }, [data]);
 
   return (
--- client/src/pages/Stats.js
+++ client/src/pages/Stats.js
@@ -1,10 +1,96 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Chart from '../components/Chart';
+import userAPI from '../utils/userAPI';
+import Typography from '@material-ui/core/Typography';
 
 const Stats = () => {
+  const [data, setData] = useState([
+    { mood: 'Happy', value: 0 },
+    { mood: 'Angry', value: 0 },
+    { mood: 'Anxious', value: 0 },
+    { mood: 'Loved', value: 0 },
+    { mood: 'Sad', value: 0 },
+  ]);
+
+  const getUserID = () => {
+    const { _id } = JSON.parse(localStorage.getItem('user'));
+    return _id;
+  };
+
+  const getUserMoods = async () => {
+    try {
+      const { data } = await userAPI.getUserData(getUserID());
+      const allUserPosts = data.posts;
+      const moodCount = { Happy: 0, Angry: 0, Anxious: 0, Loved: 0, Sad: 0 };
+
+      if (allUserPosts) {
+        allUserPosts.forEach((post) => {
+          switch (post.mood) {
+            case 'Happy':
+              moodCount.Happy++;
+              break;
+            case 'Angry':
+              moodCount.Angry++;
+              break;
+            case 'Anxious':
+              moodCount.Anxious++;
+              break;
+            case 'Loved':
+              moodCount.Loved++;
+              break;
+            case 'Sad':
+              moodCount.Sad++;
+              break;
+          }
+        });
+      }
+      return moodCount;
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const setMoodData = async () => {
+    try {
+      const moods = await getUserMoods();
+      setData([
+        { mood: 'Happy', value: moods.Happy },
+        { mood: 'Angry', value: moods.Angry },
+        { mood: 'Anxious', value: moods.Anxious },
+        { mood: 'Loved', value: moods.Loved },
+        { mood: 'Sad', value: moods.Sad },
+      ]);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const isZeroCount = () => {
+    // loop through data state
+    for (let i = 0; i < data.length; i++) {
+      // if any mood has value > 0, then return false
+      if (data[i].value !== 0) {
+        return false;
+      }
+    }
+    // otherwise return true
+    return true;
+  };
+
+  useEffect(() => {
+    setMoodData();
+  }, []);
+
   return (
-    <div className='chart-div' style={styles.container}>
-      <Chart />
+    <div className='chart-area' style={styles.container}>
+      {isZeroCount() ? (
+        <Typography style={styles.nodata}>
+          No data! <br />
+          Try logging your mood in the Moodboard!
+        </Typography>
+      ) : (
+        <Chart data={data} />
+      )}
     </div>
   );
 };
@@ -17,5 +103,11 @@ const styles = {
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
+    marginLeft: '15px',
+  },
+  nodata: {
+    fontFamily: 'ruluko',
+    textAlign: 'center',
+    lineHeight: 3,
   },
 };
